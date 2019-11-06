b61194755d17cb32cf34acd067ec53826f515f4a
Quentin Lurkin <qlurkin@gmail.com>
2019-11-06 12:15:26 +0100

    Big Bang

---
+++ README.md
@@ -0,0 +1,17 @@
+# Vulkan Init
+
+## build
+
+Creating project files:
+
+```
+mkdir build
+cd build
+cmake ..
+```
+
+building *(in the `build` folder)*:
+
+```
+cmake --build .
+```
---
+++ vulkaninit.cpp
@@ -0,0 +1,39 @@
+#include <vulkan/vulkan.hpp>
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <iostream>
+
+int main() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+	uint32_t extensionCount = 0;
+
+	std::vector<vk::ExtensionProperties> properties = vk::enumerateInstanceExtensionProperties(nullptr);
+
+	std::cout << properties.size() << " extensions supported" << std::endl;
+
+	glm::mat4 matrix;
+	glm::vec4 vec;
+	auto test = matrix * vec;
+
+	while(!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+
+	return 0;
+}
